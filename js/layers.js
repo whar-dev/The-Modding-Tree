e212f743145c8b4b5a1bc55b5228a040b80bc353
whar-dev <spookxy0@gmail.com>
2025-05-31 15:00:17 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -24,5 +24,9 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+    upgrades: {
+        11: {
+            
+        },
 })
