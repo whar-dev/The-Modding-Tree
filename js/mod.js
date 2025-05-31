2c6bf7fc114123ef09b70764b969039afe60fb3f
whar-dev <spookxy0@gmail.com>
2025-05-31 15:13:24 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -40,7 +40,7 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-
+	if (hasUpgrade('p', 11)) gain = gain.times(2)
 	let gain = new Decimal(1)
 	return gain
 }
